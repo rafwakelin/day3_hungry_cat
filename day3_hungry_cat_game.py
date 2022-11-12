49ca2e35e8a27bcdf9abf34f6a33ef498004d0d2
Rafael Queiroz <wrafael.queiroz@icloud.com>
2022-11-12 11:58:52 +0400

    Update day3_hungry_cat_game.py

--- day3_hungry_cat_game.py
+++ day3_hungry_cat_game.py
@@ -22,10 +22,6 @@ print('''
 print("Welcome to The Hungry Cat.")
 print("Your mission is to find the cat food.\n") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-#Write your code below this line 👇
-
 food = input("You didn't eat for hours and you are starving. Choose between Sheeba or Fancy Feast:\n")
 food_l = food.lower()
 if food_l == "sheeba":
